fc18a815f0a7254819f9de743987076c61877d89
nehemiah <igbennehemiah@gmail.com>
2022-05-25 16:56:00 +0100

    used book router and middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from "express";
 import express from "express";
+import "express-async-errors";
+import connectDB from "./config/database";
+import { bookRouter } from "./api/v1/routes";
+import { notFound, errorHandlerMiddleware } from "./api/v1/middlewares";
+require("dotenv").config();
 
+//Middlewares
 const app = express();
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Well done!");
-});
+//Routes
+app.use("/api/v1/kjv-bible", bookRouter);
 
-app.listen(5000, () => {
-    console.log("The application is listening on port 5000!");
-});
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        if (!process.env.MONGO_URI) throw new Error();
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log("The application is listening on port 5000!");
+        });
+    } catch (err: unknown) {
+        console.log(err);
+    }
+};
+
+start();
