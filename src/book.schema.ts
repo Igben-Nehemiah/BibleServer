1d6d19dab5fcdf0c8c4b8a58971453eeca950227
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-15 19:18:55 +0000

    created book schema for graphql

---
+++ src/book.schema.ts
@@ -0,0 +1,47 @@
+import { GraphQLInterfaceType, 
+    GraphQLList, 
+    GraphQLNonNull, 
+    GraphQLObjectType, 
+    GraphQLSchema, 
+    GraphQLString } from "graphql";
+import { getBookByName } from "./books.data";
+
+const BookType : GraphQLInterfaceType = new GraphQLInterfaceType({
+    name: "Book",
+    description: "A book of the bible",
+    fields: () => ({
+        name: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: "The name of the book"
+        },
+        abbrev: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: "The abbrevation of the book"
+        },
+        chapters: {
+            type: new GraphQLList(new GraphQLList(GraphQLString))
+        }
+    })
+});
+
+const queryType = new GraphQLObjectType({
+    name: "Query",
+    fields: () => ({
+        book: {
+            type: BookType,
+            args: {
+                name: {
+                    description: "Name of book",
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve: (_source, { name }) => getBookByName(name)
+        },
+    })
+})
+
+
+export const BookSchema: GraphQLSchema = new GraphQLSchema({
+    query: queryType,
+    types: [BookType]
+})
