20f8c518f1c3787f62aab64122e51b14cdf72e2c
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-16 16:25:31 +0000

    created env validation

---
+++ src/utils/validateEnv.ts
@@ -0,0 +1,13 @@
+import {
+    cleanEnv, port, str
+} from "envalid";
+
+
+function validateEnv() {
+    cleanEnv(process.env, {
+        PORT: port(),
+        MONGO_URI: str()
+    })
+}
+
+export default validateEnv;
