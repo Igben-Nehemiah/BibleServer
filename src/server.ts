54248b3089081a084231e8f34b943974108b25da
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-16 17:45:15 +0000

    updated server

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,45 @@
-import { Request, Response } from "express";
-import express from "express";
-import "express-async-errors";
-import connectDB from "./config/database";
-import { bookRouter } from "./api/v1/routes";
-import { notFound, errorHandlerMiddleware } from "./api/v1/middlewares";
-require("dotenv").config();
+// import { Request, Response } from "express";
+// import express from "express";
+// import "express-async-errors";
+// import connectDB from "./config/database";
+// import { bookRouter } from "./api/v1/routes";
+// import { notFound, errorHandlerMiddleware } from "./api/v1/middlewares";
+// require("dotenv").config();
 
-//Middlewares
-const app = express();
-app.use(express.json());
+import App from "./app";
+import BooksController from "./api/v1/controllers1/booksController";
+import validateEnv from "./utils/validateEnv";
 
-//Routes
-app.use("/api/v1/kjv-bible", bookRouter);
+// //Middlewares
+// const app = express();
+// app.use(express.json());
 
-app.use(notFound);
-app.use(errorHandlerMiddleware);
+// //Routes
+// app.use("/api/v1/kjv-bible", bookRouter);
 
-const port = process.env.PORT || 5000;
+// app.use(notFound);
+// app.use(errorHandlerMiddleware);
 
-const start = async () => {
-    try {
-        if (!process.env.MONGO_URI) throw new Error();
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-            console.log(`The application is listening on port ${process.env.PORT}!`);
-        });
-    } catch (err: unknown) {
-        console.log(err);
-    }
-};
+// const port = process.env.PORT || 5000;
 
-start();
+// const start = async () => {
+//     try {
+//         if (!process.env.MONGO_URI) throw new Error();
+//         await connectDB(process.env.MONGO_URI);
+//         app.listen(port, () => {
+//             console.log(`The application is listening on port ${process.env.PORT}!`);
+//         });
+//     } catch (err: unknown) {
+//         console.log(err);
+//     }
+// };
+
+// start();
+
+validateEnv();
+
+const app = new App([
+    new BooksController()
+], 4000)
+
+app.listen();
