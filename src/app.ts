409e2bee2c4c6a7bff5968c4862a284555c9b87b
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-16 16:25:15 +0000

    updated references

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import * as express from "express";
+import express from "express";
 import * as bodyParser from "body-parser";
-import { IController } from "./interfaces";
+import IController from "./api/v1/interfaces/controller.interface";
+import mongoose from "mongoose";
 
 class App {
     public app: express.Application;
@@ -11,24 +12,31 @@ class App {
         this.app = express();
         this.port = port;
 
-        this.initiliseMiddlewares();
+        this.connectToDatabase();
+        this.initialiseMiddlewares();
         this.initialiseControllers(controllers);
-    }
-
-    private initialiseMiddlewares() {
-        this.app.use(bodyParser.json());
-    }
-
-    private initialiseControllers(controllers){
-        controllers.forEach(controller => {
-            this.app.use("/", controller.router)
-        });
     }
 
     public listen() {
         this.app.listen(this.port, () => {
             console.log(`App is listening on port ${this.port}`);
         })
+    }
+
+    private initialiseMiddlewares() {
+        this.app.use(bodyParser.json());
+    }
+
+    private initialiseControllers(controllers: IController[]){
+        controllers.forEach(controller => {
+            this.app.use("/", controller.router)
+        });
+    }
+
+    private connectToDatabase() {
+        const { MONGO_URI } = process.env;
+
+        mongoose.connect(MONGO_URI || "");
     }
 }
 
