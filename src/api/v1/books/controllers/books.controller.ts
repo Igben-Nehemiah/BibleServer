ebe59a01519252e82ca50e6a824d5c573dde9b49
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-17 12:47:46 +0000

    updated books controller

--- src/api/v1/books/controllers/books.controller.ts
+++ src/api/v1/books/controllers/books.controller.ts
@@ -1,8 +1,9 @@
 import * as express from "express";
 import ControllerBase from "../../common/controller/base.controller";
+import BooksService from "../services/books.service";
 
 class BooksController extends ControllerBase {
-    constructor() {
+    constructor(private readonly booksService: BooksService) {
         super();
         this.path = "/books";
         this.initiliseRoutes();
@@ -12,8 +13,14 @@ class BooksController extends ControllerBase {
         this.router.get(this.path, this.getAllBooks.bind(this));
     }
 
-    getAllBooks = (request: express.Request, response: express.Response) => {
-        response.send("books");
+    getAllBooks = async (request: express.Request, response: express.Response) => {
+        const result = await this.booksService.getAllBooks();
+        
+        if (result.isSuccess){
+            return response.send({"books": result.data});
+        }
+
+        response.send("");
     }
 }
 
