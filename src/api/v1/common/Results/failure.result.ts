21020e8233679181fe46e228d58c86522cc4e1cb
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-17 12:50:15 +0000

    updated failure result model

--- src/api/v1/common/Results/failure.result.ts
+++ src/api/v1/common/Results/failure.result.ts
@@ -1,9 +1,8 @@
 import Result from "./base.result";
 
 class FailureResult<T> extends Result<T> {
-    constructor(data: T, 
-        errors: string | string[] | null) {
-        super(data, false, errors);
+    constructor(errors: string | string[] | null) {
+        super(null, false, errors);
     }
 }
 
