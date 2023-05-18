cc81c525e12e7316613eb9add7c70fd1c327273d
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 17:20:01 +0000

    added token data and id to user interface

---
+++ src/api/modules/authentication/interfaces/token-data.ts
@@ -0,0 +1,10 @@
+
+export interface TokenData {
+    token: string;
+    expiresIn: number;
+}
+
+
+export interface DataStoredInToken {
+    _id?: string;
+}
--- src/api/modules/authentication/interfaces/user.interface.ts
+++ src/api/modules/authentication/interfaces/user.interface.ts
@@ -1,7 +1,9 @@
 interface User {
+    _id?: string;
     name?: string;
     email?: string;
     password?: string;
 }
 
 export default User;
+
