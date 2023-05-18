a70f10dec7e5fb42c3e6051c11aa91b9ad526e38
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 16:40:51 +0000

    cleaned up server startup

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,19 @@
-// import { Request, Response } from "express";
-// import express from "express";
-// import "express-async-errors";
-// import connectDB from "./config/database";
-// import { bookRouter } from "./api/v1/routes";
-// import { notFound, errorHandlerMiddleware } from "./api/v1/middlewares";
-// require("dotenv").config();
-
+import BooksController from "./api/v1/books/books.controller";
+import BooksService from "./api/v1/books/books.service";
+import BookModel from "./api/v1/books/models/book.model";
+import BooksRepository from "./api/v1/books/repositories/books.repository";
 import App from "./app";
-import BooksController from "./api/v1/controllers1/booksController";
 import validateEnv from "./utils/validateEnv";
 
-// //Middlewares
-// const app = express();
-// app.use(express.json());
-
-// //Routes
-// app.use("/api/v1/kjv-bible", bookRouter);
-
-// app.use(notFound);
-// app.use(errorHandlerMiddleware);
-
-// const port = process.env.PORT || 5000;
-
-// const start = async () => {
-//     try {
-//         if (!process.env.MONGO_URI) throw new Error();
-//         await connectDB(process.env.MONGO_URI);
-//         app.listen(port, () => {
-//             console.log(`The application is listening on port ${process.env.PORT}!`);
-//         });
-//     } catch (err: unknown) {
-//         console.log(err);
-//     }
-// };
-
-// start();
 
 validateEnv();
 
+// TODO: This should be done in a DI container or something
+const booksRepository = new BooksRepository(BookModel);
+const booksService = new BooksService(booksRepository);
+
 const app = new App([
-    new BooksController()
+    new BooksController(booksService)
 ], 4000)
 
 app.listen();
