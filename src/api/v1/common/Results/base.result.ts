4e06ccfc6ca446d75104cdabec3cdd55593df9f3
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 16:33:15 +0000

    added BaseResult

--- src/api/v1/common/Results/base.result.ts
+++ src/api/v1/common/Results/base.result.ts
@@ -1,15 +1,28 @@
-class Result<T>{
+export class BaseResult{
+    public readonly isFailure: boolean;
+
+    constructor(
+        public readonly isSuccess: boolean,
+        public readonly error: string | null  = null
+        ) {
+        this.error = error;
+        this.isSuccess = isSuccess;
+        this.isFailure = !this.isSuccess;
+    }
+}
+
+class Result<T> extends BaseResult{
     public readonly isFailure: boolean;
 
     constructor(
         public readonly data: T | null,
         public readonly isSuccess: boolean,
-        public readonly errors: string | string[] | null  = null
+        public readonly error: string | null  = null
         ) {
-        this.errors = errors;
-        this.isSuccess = isSuccess;
+        super(isSuccess, error)
         this.isFailure = !this.isSuccess;
     }
 }
+
 
 export default Result;
