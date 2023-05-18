08ee5c2325925edd57a63781c15d7f910a6afc34
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 16:31:30 +0000

    added create user dto

---
+++ src/api/v1/authentication/dtos/createUser.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+class CreateUserDto {
+    @IsNotEmpty()
+    @IsEmail()
+    public email!: string;
+
+    @IsNotEmpty()
+    public password!: string;
+}
+
+export default CreateUserDto;
