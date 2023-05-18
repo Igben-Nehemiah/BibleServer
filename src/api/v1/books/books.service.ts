fcb724cf125a885cc67ab7a905ba68fd7b9cb792
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 16:30:57 +0000

    moved book service to books dir

--- src/api/v1/books/books.service.ts
+++ src/api/v1/books/books.service.ts
@@ -1,8 +1,8 @@
-import Result from "../../common/Results/base.result";
-import FailureResult from "../../common/Results/failure.result";
-import SuccessResult from "../../common/Results/success.result";
-import { IBook } from "../interfaces";
-import BooksRepository from "../repositories/books.repository";
+import Result from "../common/Results/base.result";
+import FailureResult from "../common/Results/failure.result";
+import SuccessResult from "../common/Results/success.result";
+import { IBook } from "./interfaces";
+import BooksRepository from "./repositories/books.repository";
 
 class BooksService {
     constructor(private readonly booksRepository: BooksRepository) {
@@ -14,16 +14,16 @@ class BooksService {
             return Promise.resolve(new SuccessResult(books));
         }
         catch(e: unknown) {
-            return Promise.resolve(new FailureResult(["Failed to get books!"]));
+            return Promise.resolve(new FailureResult("Failed to get books!"));
         }
     }
 
     async getBookByName(name: string): Promise<Result<IBook>> {
         try {
             const book = await this.booksRepository.getBookByName(name);
-            return Promise.resolve(new SuccessResult(book));
+            return new SuccessResult(book);
         }catch(e: unknown){
-            return Promise.resolve(new FailureResult(["Failed to get book"]));
+            return new FailureResult("Failed to get book");
         }
     }
 }
