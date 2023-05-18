d2310c16db472011c9d602c67087d4e42e8c6d1c
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 16:31:19 +0000

    implemented authentication controller and service

--- src/api/v1/authentication/authentication.controller.ts
+++ src/api/v1/authentication/authentication.controller.ts
@@ -1,8 +1,49 @@
 import * as express from "express";
 import IController from "../common/interfaces/controller.interface";
+import AuthenticationService from "./authentication.service";
+import CreateUserDto from "./dtos/createUser.dto";
+import LoginDto from "./dtos/login.dto";
+import { HttpException } from "../common/errors/custom-error";
+import validationMiddleware from "../middlewares/validation.middleware";
 
 class AuthenticationController implements IController {
     public router = express.Router();
     public path = "/auth";
 
-}
+    constructor(private readonly authService: AuthenticationService) {
+        this.initialiseRoutes();
+    }
+
+    private initialiseRoutes() {
+        this.router.post(this.path + "/login", validationMiddleware(LoginDto), this.logIn);
+        this.router.post(this.path + "/signup", validationMiddleware(CreateUserDto), this.registerUser);
+    };
+
+    private registerUser = async (request: express.Request, 
+        response: express.Response,
+        next: express.NextFunction) => {
+        const userData: CreateUserDto = request.body;
+
+        const result = await this.authService.registerUser(userData);
+        if (result.isSuccess){
+            return response.status(201).json(result.data);
+        }
+        throw new HttpException(result.error || "", 400); // Todo: Look at this implementation later
+    };
+    
+    private logIn = async (request: express.Request, 
+        response: express.Response,
+        next: express.NextFunction) => {
+        
+        const userData: LoginDto = request.body;
+
+        const result = await this.authService.login(userData);
+
+        if (result.isSuccess) {
+            return response.status(200).json(result.data);
+        }
+        throw new HttpException(result.error || "", 400);
+    };
+};
+
+export default AuthenticationController;
---
+++ src/api/v1/authentication/authentication.service.ts
@@ -0,0 +1,68 @@
+import { isInstance } from "class-validator";
+import Result from "../common/Results/base.result";
+import FailureResult from "../common/Results/failure.result";
+import SuccessResult from "../common/Results/success.result";
+import throwIfNullOrUndefined from "../common/guards/nullAndUndefined.guard";
+import CreateUserDto from "./dtos/createUser.dto";
+import UserWithEmailAlreadyExistsException from "./exceptions/userWithEmailAlreadyExists.exception";
+import User from "./interfaces/user.interface";
+import AuthenticationRepository from "./repositories/authentication.repository";
+import * as bcrypt from "bcrypt";
+import LoginDto from "./dtos/login.dto";
+import WrongCredentialsException from "./exceptions/wrongCredentials.exception";
+import { HttpException } from "../common/errors/custom-error";
+
+
+class AuthenticationService {
+    constructor(private readonly authRepository: AuthenticationRepository) {};
+
+    public async registerUser(createUserDto: CreateUserDto) : Promise<Result<User>> {
+        try {
+            throwIfNullOrUndefined(createUserDto.email);
+            throwIfNullOrUndefined(createUserDto.password);
+
+            const existingUser = await this.authRepository.findUserByEmail(createUserDto.email!);
+
+            if (existingUser) throw new UserWithEmailAlreadyExistsException(createUserDto.email!);
+
+            const hashedPassword = await bcrypt.hash(createUserDto.password!, 5);
+
+            const newUser = await this.authRepository.add({
+                ...createUserDto,
+                password: hashedPassword
+            });
+
+            delete newUser.password;
+            return new SuccessResult(newUser);
+            
+        }catch(e: unknown){
+            return isInstance(e, Error) ? new FailureResult([(e as Error).message]) 
+                : new FailureResult(["User registration failed"]);
+        }
+    };
+
+    public async login(loginDto: LoginDto) : Promise<Result<User>> {
+        try {
+
+            throwIfNullOrUndefined(loginDto.email);
+            throwIfNullOrUndefined(loginDto.password);
+    
+            const user = await this.authRepository.findUserByEmail(loginDto.email!);
+    
+            if (!user) throw new WrongCredentialsException();
+    
+            const isPasswordMatching = await bcrypt.compare(loginDto.password!, user.password!);
+
+            if (!isPasswordMatching) throw new WrongCredentialsException();
+
+            delete user.password;
+
+            return new SuccessResult(user);
+        }catch(e: unknown){
+            return isInstance(e, Error) || isInstance(e, HttpException) ? new FailureResult([(e as Error).message]) 
+                : new FailureResult(["User login failed"]);
+        }
+    }
+}
+
+export default AuthenticationService;
