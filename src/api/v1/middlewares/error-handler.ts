e25f10f34b37068cec706b609a4e7c14d6fb34f8
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 13:21:20 +0000

    updated error handler

--- src/api/v1/middlewares/error-handler.ts
+++ src/api/v1/middlewares/error-handler.ts
@@ -1,19 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import { CustomAPIError } from "../common/errors/custom-error";
+import { HttpException } from "../common/errors/custom-error";
 import FailureResult from "../common/Results/failure.result";
 
 const errorHandlerMiddleware = (
     err: any,
     req: Request,
     res: Response,
-    next: NextFunction
-) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json(new FailureResult([err.message]));
+    next: NextFunction) => {
+        
+    if (err instanceof HttpException) {
+        res.status(err.statusCode).send(new FailureResult([err.message]));
     }
-    return res
-        .status(500)
-        .json(new FailureResult(["Something went wrong, please try again"]));
+    res.status(500)
+        .send(new FailureResult(["Something went wrong, please try again"]));
 };
 
 export default errorHandlerMiddleware;
