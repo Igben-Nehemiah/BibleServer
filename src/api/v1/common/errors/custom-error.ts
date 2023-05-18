dff93ae10bc87c25cb0007fe475f9c780fcadd99
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 13:20:55 +0000

    changed CustomApiError to HttpException

--- src/api/v1/common/errors/custom-error.ts
+++ src/api/v1/common/errors/custom-error.ts
@@ -1,11 +1,11 @@
-class CustomAPIError extends Error {
+class HttpException extends Error {
     constructor(message: string, public statusCode: number) {
         super(message);
     }
 }
 
 const createCustomError = (msg: string, statusCode: number) => {
-    return new CustomAPIError(msg, statusCode);
+    return new HttpException(msg, statusCode);
 };
 
-export { createCustomError, CustomAPIError };
+export { createCustomError, HttpException };
