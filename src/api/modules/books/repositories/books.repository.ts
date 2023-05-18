fad5c2ed03120bcd3e4986c30cf62240c1a5fd34
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-18 17:03:13 +0000

    added new method to get all books with book name query

--- src/api/modules/books/repositories/books.repository.ts
+++ src/api/modules/books/repositories/books.repository.ts
@@ -1,21 +1,37 @@
 import BaseRepository from "../../../common/repositories/base.repository";
-import { IBook, IBookRepository as IBooksRepository } from "../interfaces";
+import { Book, IBookRepository as IBooksRepository } from "../interfaces";
 import BookModel from "../models/book.model";
 
 class BooksRepository 
-    extends BaseRepository<IBook>
+    extends BaseRepository<Book>
     implements IBooksRepository {
     
     constructor(model: typeof BookModel) {
         super(model)
     }
 
-    async getBookByName(name: string): Promise<IBook>{
+    async getBookByName(name: string): Promise<Book>{
         const book = await this.model.findOne({name});
         
         if (!book) throw new Error(`${name} not found!`);
 
-        return Promise.resolve(book as IBook);
+        return Promise.resolve(book as Book);
+    }
+
+    async getAllBooks(name?: string): Promise<Book[]> {
+        const queryObject: {
+            name?: {
+                $regex: any;
+                $options: string;
+            };
+        } = {};
+    
+        if (name) {
+            queryObject.name = { $regex: name, $options: "i" };
+        }
+
+        const books = await this.model.find(queryObject);
+        return books as Book[];
     }
 }
 
