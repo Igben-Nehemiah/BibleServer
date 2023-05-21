5c9495c1c7c5fc8d9796a88a137d821121fcd6fc
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-21 23:42:37 +0100

    updated user repository

--- src/api/modules/authentication/repositories/authentication.repository.ts
+++ src/api/modules/authentication/repositories/authentication.repository.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 import BaseRepository from "../../../common/repositories/base.repository";
 import User from "../interfaces/user.interface";
+import IAuthenticationRepository from "../interfaces/authentication-repository.interface";
 
 
-class AuthenticationRepository extends BaseRepository<User>{
+class AuthenticationRepository extends BaseRepository<User> 
+    implements IAuthenticationRepository {
     constructor(public readonly model:  mongoose.Model<User & mongoose.Document>) {
         super(model);
     }
 
     public async findUserByEmail(email: string) : Promise<User | null> {
         const user = await this.model.findOne({email});
-
         return user as User;
     }
 }
