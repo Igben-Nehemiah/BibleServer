99c478889405a7ce0c001480fcef5ce7b7150d7c
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-23 16:03:12 +0000

    added authentication controller test

---
+++ src/api/modules/authentication/tests/authentication.controller.test.ts
@@ -0,0 +1,44 @@
+import App from "../../../../app";
+import AuthenticationController from "../authentication.controller";
+import AuthenticationService from "../authentication.service";
+import CreateUserDto from "../dtos/create-user.dto";
+import IAuthenticationRepository from "../interfaces/authentication-repository.interface";
+import * as request from 'supertest';
+
+
+const authRepository : IAuthenticationRepository = {
+    add: jest.fn().mockReturnValue({}),
+    findUserByEmail: jest.fn().mockReturnValue({
+        _id: "61505d4e7a7e6c001f2ce301",
+        name: "Test Test",
+        email: "test@test.com",
+    }),
+    getAll: jest.fn().mockReturnValue({}),
+    getById: jest.fn().mockReturnValue({}),
+    remove: jest.fn().mockReturnValue({}),
+}
+
+describe("AuthenticationController", () => {
+    describe("POST /auth/signup", () => {
+        describe('if the email is not taken', () => {
+            it('response should have the Set-Cookie header with the Authorization token', () => {
+              const userData: CreateUserDto = {
+                email: 'john@smith.com',
+                password: 'strongPassword123',
+              };
+              process.env.JWT_SECRET = 'jwt_secret';
+
+              const authenticationService = new AuthenticationService(authRepository)
+              const authenticationController = new AuthenticationController(authenticationService);
+
+              const app = new App([
+                authenticationController,
+              ], 3030);
+              return request.agent(app)
+                .post(`${authenticationController.path}/register`)
+                .send(userData)
+                .expect('Set-Cookie', /^Authorization=.+/)
+            })
+        });
+    });
+});
