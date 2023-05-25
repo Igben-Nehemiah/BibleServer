f66b5f06e35ab661ba863f8162261dbc18bb9d63
Igben-Nehemiah <78425633+Igben-Nehemiah@users.noreply.github.com>
2023-05-25 17:34:35 +0000

    updated authentication controller

--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -2,3 +2,4 @@ import notFound from "./not-found";
 import errorHandlerMiddleware from "./error-handler";
 
 export { notFound, errorHandlerMiddleware };
+export * from "./authentication.middleware";
