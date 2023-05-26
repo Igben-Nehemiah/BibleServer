fdcc27f6ba268a0fc02d094a9f58583f1ee2deaa
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-26 09:27:49 +0100

    updated authentication middleware

--- src/api/middlewares/authentication.middleware.ts
+++ src/api/middlewares/authentication.middleware.ts
@@ -1,35 +1,33 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, RequestHandler, Response } from "express";
 import * as jwt from "jsonwebtoken";
 import { DataStoredInToken, RequestWithUser, User } from "../modules/authentication/interfaces";
 import userModel from "../modules/authentication/models/user.model";
-import WrongAuthenticationTokenException from "../modules/authentication/exceptions/wrong-authentication-token.exception";
+import { AuthenticationTokenMissingException, WrongAuthenticationTokenException } from "../modules/authentication/exceptions";
 
-export async function authenticationMiddleware(
-    request: RequestWithUser, 
-    response: Response, 
-    next: NextFunction){
-    const cookies = request.cookies;
-
-    if (cookies && cookies.Authorization){
-        const secret = process.env.JWT_SECRET!;
-
+export function authenticationMiddleware(omitSecondFactor = false): any {
+    return async (request: RequestWithUser, response: Response, next: NextFunction) => {
+      const cookies = request.cookies;
+      if (cookies && cookies.Authorization) {
+        const secret = process.env.JWT_SECRET || "";
         try {
-            const verificationResponse = 
-                jwt.verify(cookies.Authorization, secret) as DataStoredInToken;
-            const id = verificationResponse._id;
-
-            const user = await userModel.findById(id);
-
-            if (user) {
-                request.user = user;
-                next();
+          const verificationResponse = jwt.verify(cookies.Authorization, secret) as DataStoredInToken;
+          const { _id: id, isSecondFactorAuthenticated } = verificationResponse;
+          const user = await userModel.findById(id);
+          if (user) {
+            if (!omitSecondFactor && user.isTwoFactorAuthenticationEnabled && !isSecondFactorAuthenticated) {
+              next(new WrongAuthenticationTokenException());
             } else {
-                next(new WrongAuthenticationTokenException());
-            };
-        } catch(e: unknown){
+              request.user = user;
+              next();
+            }
+          } else {
             next(new WrongAuthenticationTokenException());
-        };
-    } else {
-        next(new WrongAuthenticationTokenException());
+          }
+        } catch (error) {
+          next(new WrongAuthenticationTokenException());
+        }
+      } else {
+        next(new AuthenticationTokenMissingException());
+      }
     };
-};
+  }
