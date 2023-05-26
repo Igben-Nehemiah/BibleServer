a4b12364387e0ccbd73eb58018e110a3610700f0
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-26 09:30:05 +0100

    updated export pattern

--- src/api/modules/authentication/exceptions/user-with-email-already-exists.exception.ts
+++ src/api/modules/authentication/exceptions/user-with-email-already-exists.exception.ts
@@ -1,10 +1,7 @@
 import { HttpException } from "../../../common/errors/custom-error";
 
-class UserWithEmailAlreadyExistsException extends HttpException {
+export class UserWithEmailAlreadyExistsException extends HttpException {
     constructor(email: string) {
         super(`${email} has already been used`, 400);
-    }
+    };
 };
-
-
-export default UserWithEmailAlreadyExistsException;
--- src/api/modules/authentication/exceptions/wrong-credentials.exception.ts
+++ src/api/modules/authentication/exceptions/wrong-credentials.exception.ts
@@ -1,10 +1,7 @@
 import { HttpException } from "../../../common/errors/custom-error";
 
-
-class WrongCredentialsException extends HttpException {
+export class WrongCredentialsException extends HttpException {
     constructor() {
         super("Wrong credentials provided!", 401);
     }
 };
-
-export default WrongCredentialsException;
