690b555e4c33006393b29e602ff4a87c6f71fbe3
Nehemiah Igben <nehemiah.igben@cyphercrescent.com>
2023-05-30 16:25:25 +0100

    added response interceptor

---
+++ src/api/middlewares/response-interceptor.middleware.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import * as express from 'express';
+import { Response } from '../common/responses';
+
+export const responseInterceptor = (
+  request: express.Request,
+  response: express.Response,
+  next: express.NextFunction
+) => {
+  const originalSend = response.send;
+
+  // Remove null value members of response object
+  response.send = function (body: any) {
+    if (body instanceof Response) {
+      for (const key in body) {
+        if (Object.prototype.hasOwnProperty.call(body, key)) {
+          if ((body as any)[key] == null) {
+            delete (body as any)[key];
+          }
+        }
+      }
+    }
+
+    const modifiedBody = body;
+    return originalSend.call(this, modifiedBody);
+  };
+  next();
+};
