4bb2c68ceb46e9d9aa8abb8ccd04de4f6b851d85
Nehemiah Igben <nehemiah.igben@cyphercrescent.com>
2023-05-30 16:25:46 +0100

    updated server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import { populateBooks } from '../populate';
+import AuthenticationController from './api/modules/authentication/authentication.controller';
+import AuthenticationService from './api/modules/authentication/authentication.service';
+import UserModel from './api/modules/authentication/models/user.model';
+import AuthenticationRepository from './api/modules/authentication/repositories/authentication.repository';
 import BooksController from './api/modules/books/books.controller';
 import BooksService from './api/modules/books/books.service';
 import BookModel from './api/modules/books/models/book.model';
@@ -8,11 +13,19 @@ import validateEnv from './utils/validate-env';
 
 validateEnv();
 connectToDatabase();
+// populateBooks();
+
+const port = parseInt(`${process.env.PORT ?? 4000}`);
 
 // TODO: This should be done in a DI container or something
 const booksRepository = new BooksRepository(BookModel);
 const booksService = new BooksService(booksRepository);
+const booksController = new BooksController(booksService);
 
-const app = new App([new BooksController(booksService)], 4000);
+const authRepository = new AuthenticationRepository(UserModel);
+const authService = new AuthenticationService(authRepository);
+const authController = new AuthenticationController(authService);
+
+const app = new App([authController, booksController], port);
 
 app.listen();
