c47e7541d1f29c6a010a5993ba6f62fe4806662c
Nehemiah Igben <nehemiah.igben@cyphercrescent.com>
2023-05-30 11:56:26 +0100

    updated imports

--- src/api/modules/books/books.service.ts
+++ src/api/modules/books/books.service.ts
@@ -1,7 +1,6 @@
-import { type Result } from '@nehemy/result-monad';
+import { Result } from '@nehemy/result-monad';
 import { type Book } from './interfaces';
 import type BooksRepository from './repositories/books.repository';
-import { Ok } from '../../common/responses';
 
 class BooksService {
   constructor(private readonly booksRepository: BooksRepository) {}
@@ -9,18 +8,20 @@ class BooksService {
   async getAllBooks(): Promise<Result<Book[]>> {
     try {
       const books = await this.booksRepository.getAll();
-      return await Promise.resolve(new Ok(books));
+      return await Promise.resolve(new Result(books));
     } catch (e: unknown) {
-      return await Promise.resolve(new FailureResult('Failed to get books!'));
+      return await Promise.resolve(
+        new Result<Book[]>(new Error('Failed to get books!'))
+      );
     }
   }
 
   async getBookByName(name: string): Promise<Result<Book>> {
     try {
       const book = await this.booksRepository.getBookByName(name);
-      return new Ok(book);
+      return new Result(book);
     } catch (e: unknown) {
-      return new FailureResult('Failed to get book');
+      return new Result<Book>(new Error('Failed to get book'));
     }
   }
 }
