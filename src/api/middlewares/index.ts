b275af7d3a8acfbed6bc58a55eab113213380efd
Nehemiah Igben <nehemiah.igben@cyphercrescent.com>
2023-05-30 16:26:17 +0100

    added response interceptor to barrel exports

--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -4,3 +4,4 @@ import errorHandlerMiddleware from './error-handler';
 export { notFound, errorHandlerMiddleware };
 export * from './authentication.middleware';
 export * from './validation.middleware';
+export * from './response-interceptor.middleware';
