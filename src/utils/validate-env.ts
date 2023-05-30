c6c1008f788ae0ca4f1f002a3769da49519c7588
Nehemiah Igben <nehemiah.igben@cyphercrescent.com>
2023-05-30 11:54:38 +0100

    added more validations

--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -1,9 +1,16 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, num, port, str } from 'envalid';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 function validateEnv() {
   cleanEnv(process.env, {
     PORT: port(),
     MONGO_URI: str(),
+    SALT_ROUNDS: num(),
+    JWT_SECRET: str(),
+    JWT_ALGORITHM: str(),
+    TWO_FACTOR_AUTHENTICATION_APP_NAME: str(),
   });
 }
 
