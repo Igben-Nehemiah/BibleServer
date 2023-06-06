37349a8d3ed658bfc9dadd66a755b10e43875b44
Nehemiah Igben <igbennehemiah@gmail.com>
2023-06-06 22:07:23 +0100

    included endpoint to utilise graphql

--- src/api/graphql/graphql-server.ts
+++ src/api/graphql/graphql-server.ts
@@ -1,30 +1,41 @@
 import { graphql } from 'graphql';
 import { BookSchema } from './book.schema';
-import { Book } from './books.data';
+import { Book, getBookByName } from './books.data';
+import express from 'express';
+import * as bodyParser from 'body-parser';
 
-const source = `
-  query GetBook($name: String!) {
-    book(name: $name) {
-      name,
-      chapters
-    }
-  }
-`;
+const app = express();
+app.use(bodyParser.json());
 
-const variables = {
-  name: 'Sample Book',
+const root = {
+  book: ({ name }: { name: string }) => {
+    const bookData = getBookByName(name);
+    return bookData;
+  },
 };
 
-graphql({ schema: BookSchema, source, variableValues: variables })
-  .then(result => {
-    if (result.data == null || result.data == undefined) return;
-    const book = result.data.book as Book;
-    const chapters = book.chapters;
+// GraphQL endpoint
+app.post('/graphql', (req, res) => {
+  const variables = req.body.variables;
+  const query = req.body.query;
 
-    console.log('Book name: ', book.name);
-    console.log('Book:', book);
-    console.log('Chapters:', chapters);
+  graphql({
+    schema: BookSchema,
+    source: query,
+    // rootValue: root,
+    variableValues: variables,
   })
-  .catch(e => {
-    console.log(e);
-  });
+    .then(result => {
+      if (result.data == null || result.data == undefined) return;
+
+      res.json(result);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
