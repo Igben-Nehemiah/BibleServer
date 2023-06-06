821bd7a0e66c12a4bbd81914ace4527b1e21bb9d
Nehemiah Igben <igbennehemiah@gmail.com>
2023-06-06 18:46:38 +0100

    created getBookByName method

--- src/api/graphql/books.data.ts
+++ src/api/graphql/books.data.ts
@@ -1,6 +1,25 @@
 export interface Book {
   type: 'Book';
+  id: string;
   name: string;
   abbrev: string;
-  chapters: ReadonlyArray<readonly string[]>;
+  chapters: ReadonlyArray<ReadonlyArray<string>>;
+}
+
+const books: Array<Book> = [];
+
+function getBooksData(books: Array<Book>) {
+  const booksData: { [name: string]: Book } = {};
+
+  books.forEach(book => {
+    booksData[book.name] = book;
+  });
+
+  return booksData;
+}
+
+const booksData = getBooksData(books);
+
+export function getBookByName(bookName: string): Promise<Book | null> {
+  return Promise.resolve(booksData[bookName]) ?? null;
 }
