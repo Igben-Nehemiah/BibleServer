ba297c1f24da6e77f10f6e0bb683507a032cf02c
Nehemiah Igben <igbennehemiah@gmail.com>
2023-06-07 22:55:51 +0100

    made id string

--- src/api/common/repositories/base-repository.interface.ts
+++ src/api/common/repositories/base-repository.interface.ts
@@ -2,9 +2,9 @@ export interface IBaseRepository<T> {
   add: (item: T) => Promise<T>;
   remove: (item: T) => Promise<boolean>;
   getAll: () => Promise<T[]>;
-  getById: (id: string | number) => Promise<T | null>;
+  getById: (id: string) => Promise<T | null>;
   findByIdAndUpdate: (
-    id: string | number,
+    id: string,
     updatedModel: Partial<T>
   ) => Promise<T | null>;
 }
