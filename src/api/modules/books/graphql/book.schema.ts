8aaea8447a80ea38749859a0e735560f9b6b7c50
Nehemiah Igben <igbennehemiah@gmail.com>
2023-06-07 21:54:52 +0100

    moved files to graphql dir

---
+++ src/api/modules/books/graphql/book.schema.ts
@@ -0,0 +1,54 @@
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList,
+} from 'graphql';
+import { getBookByName, getBookChapters } from './books.data';
+
+const bookType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'A book of the bible',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The id of the book.',
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The name of the book',
+    },
+    abbrev: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The abbreviation of the book',
+    },
+    chapters: {
+      type: new GraphQLList(new GraphQLList(GraphQLString)),
+      resolve: book => getBookChapters(book),
+    },
+  }),
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    book: {
+      type: bookType,
+      args: {
+        name: {
+          description: 'Name of book',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_source, { name }) => {
+        return getBookByName(name);
+      },
+    },
+  }),
+});
+
+export const BookSchema: GraphQLSchema = new GraphQLSchema({
+  query: queryType,
+  types: [bookType],
+});
